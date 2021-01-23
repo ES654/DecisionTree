756bb12fe1184e741fcb671bb867e432740bfae5
varunjain3 <varun.jain@iitgn.ac.in>
2021-01-24 00:29:49 +0530

    Added functions

--- assignment-1/tree/utils.py
+++ assignment-1/tree/utils.py
@@ -8,8 +8,19 @@ def entropy(Y):
     Outpus:
     > Returns the entropy as a float
     """
-    
-    pass
+    assert(Y.size == attr.size)
+    assert(Y.size > 0)
+
+    S = 0
+    # For discrete
+    classes = Y.groupby(Y).count()
+    for cat in list(classes.index):
+        p = classes[cat]/Y.size
+        S -= p*np.log2(p)
+        print("Entropy: ", p*np.log2(p))
+
+    return S
+
 
 def gini_index(Y):
     """
@@ -20,16 +31,42 @@ def gini_index(Y):
     Outpus:
     > Returns the gini index as a float
     """
-    pass
+    cats = Y.groupby(Y).count()
+    gini = 1
+
+    for cat in list(cats.index):
+        gini -= cats[cat]/sum(cats)**2
+
+    return gini
+
 
 def information_gain(Y, attr):
     """
     Function to calculate the information gain
-    
+
     Inputs:
     > Y: pd.Series of Labels
     > attr: pd.Series of attribute at which the gain should be calculated
     Outputs:
     > Return the information gain as a float
     """
-    pass
+    assert(Y.size == attr.size)
+    assert(Y.size > 0)
+
+    if Y.dtype == "category":
+        outlooks = attr.groupby(attr).count()
+        gain = entropy(Y)
+        for outlook in list(outlooks.index):
+            Y_sub = Y[attr = outlook]
+            gain - Y_sub.size/Y.size*entropy(Y_sub)
+        print("Continuous Gain: ", gain)
+
+    else:
+        gain = np.var(Y)
+        outlooks = attr.groupby(attr).count()
+        for outlook in list(outlooks.index):
+            Y_sub = Y[attr = outlook]
+            gain - Y_sub.size/Y.size*np.var(Y_sub)
+        print("Continuous Gain: ", gain)
+
+    return gain
