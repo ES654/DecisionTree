f9dcadbd49b0e3609005c2532507f5e84e28a9f1
varunjain3 <varun.jain@iitgn.ac.in>
2021-01-30 03:36:23 +0530

    plotting added

--- assignment-1/random_forest_iris.py
+++ assignment-1/random_forest_iris.py
@@ -19,7 +19,12 @@ iris["variety"] = iris["variety"].astype("category")
 shuffled = iris.sample(frac=1).reset_index(drop=True)
 
 X = shuffled.iloc[:, :-1].squeeze()
+X = X[['sepal.width', "petal.width"]]
 y = (shuffled.iloc[:, -1:]).T.squeeze()
+y[y == "Setosa"] = "Versicolor"
+y = y.cat.remove_unused_categories()
+y = y.cat.rename_categories(["Not Verginica", "Virginica"])
+
 len_iris = len(y)
 
 X_train, y_train = X.loc[:split*len_iris], y.loc[:split*len_iris]
@@ -27,10 +32,10 @@ X_test, y_test = X.loc[split*len_iris+1:].reset_index(
     drop=True), y.loc[split*len_iris+1:].reset_index(drop=True)
 
 
-tree = RandomForestClassifier(5, criterion="information_gain")
+tree = RandomForestClassifier(10, criterion="information_gain", max_attr=2)
 tree.fit(X_train, y_train)
 y_hat = tree.predict(X_test)
-tree.plot()
+tree.plot(X, y, db=True)
 
 # Calculating Metrics
 print('Accuracy: ', accuracy(y_hat, y_test))
--- assignment-1/tree/randomForest.py
+++ assignment-1/tree/randomForest.py
@@ -1,11 +1,14 @@
 from .base import DecisionTree
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.utils.extmath import weighted_mode
 
 
 class RandomForestClassifier():
-    def __init__(self, n_estimators=100, criterion='gini', max_depth=100, max_attr=3):
+    def __init__(self, n_estimators=100, criterion='gini', max_depth=10, max_attr=3):
         '''
         :param estimators: DecisionTree
         :param n_estimators: The number of trees in the forest.
@@ -26,7 +29,8 @@ class RandomForestClassifier():
         y: pd.Series with rows corresponding to output variable (shape of Y is N)
         """
         for n in tqdm(range(self.n_estimators)):
-            X_sub = X.sample(n=self.max_attr, axis='columns')
+            X_sub = X.sample(n=np.random.randint(
+                1, self.max_attr + 1), axis='columns')
             tree = DecisionTree(criterion=self.criterion,
                                 max_depth=self.max_depth)
             tree.fit(X_sub, y)
@@ -42,13 +46,13 @@ class RandomForestClassifier():
         """
         y_hat_total = None
         for i, tree in enumerate(self.trees):
-            if y_hat_total is No3ne:
+            if y_hat_total is None:
                 y_hat_total = tree.predict(X).to_frame()
             else:
                 y_hat_total[i] = tree.predict(X)
         return y_hat_total.mode(axis=1)[0]
 
-    def plot(self):
+    def plot(self, X, y, db=False):
         """
         Function to plot for the RandomForestClassifier.
         It creates three figures
@@ -61,15 +65,74 @@ class RandomForestClassifier():
         3. Creates a figure showing the combined decision surface
 
         """
+
         for i, tree in enumerate(self.trees):
             print("-----------------------------")
             print("Tree Number: {}".format(i+1))
             print("-----------------------------")
             tree.plot()
+        if db:
+            return self.decisionBoundary(X, y)
+
+    def decisionBoundary(self, X, y):
+        assert(len(list(X.columns)) == 2)
+
+        color = ["r", "b", "g"]
+        lookup = {"Not Verginica": 0, "Virginica": 1}
+        fig1, ax1 = plt.subplots(
+            1, len(self.trees), figsize=(5*len(self.trees), 4))
+
+        x_min, x_max = X.iloc[:, 0].min(), X.iloc[:, 0].max()
+        y_min, y_max = X.iloc[:, 1].min(), X.iloc[:, 1].max()
+        x_range = x_max-x_min
+        y_range = y_max-y_min
+        Zs = []
+
+        for i, tree in enumerate(self.trees):
+            xx, yy = np.meshgrid(np.arange(x_min-0.2, x_max+0.2, (x_range)/50),
+                                 np.arange(y_min-0.2, y_max+0.2, (y_range)/50))
+            Z = tree.predict(pd.DataFrame(
+                np.c_[xx.ravel(), yy.ravel()], columns=list(X.columns))).to_numpy()
+            Z = np.vectorize(lambda x: lookup[x])(Z)
+            Z = Z.reshape(xx.shape)
+            cs = ax1[i].contourf(xx, yy, Z, cmap=plt.cm.RdYlBu)
+            ax1[i].set_ylabel("X2")
+            ax1[i].set_xlabel("X1")
+            Zs.append(Z)
+            for y_label in y.unique():
+                idx = y == y_label
+                id = list(y.cat.categories).index(y[idx].iloc[0])
+                ax1[i].scatter(X[idx].iloc[:, 0], X[idx].iloc[:, 1], c=color[id],
+                               cmap=plt.cm.RdYlBu, edgecolor='black', s=30,
+                               label="Class: "+str(y_label))
+            ax1[i].set_title("Decision Surface Tree: " + str(i+1))
+            ax1[i].legend()
+        fig1.tight_layout()
+
+        fig2, ax2 = plt.subplots(1, 1, figsize=(5, 4))
+        Zs = np.array(Zs)
+        com_surface, _ = weighted_mode(Zs, np.ones(Zs.shape))
+        Z = np.mean(Zs, axis=0)
+        cs = ax2.contourf(xx, yy, com_surface[0], cmap=plt.cm.RdYlBu)
+        for y_label in y.unique():
+            idx = y == y_label
+            id = list(y.cat.categories).index(y[idx].iloc[0])
+            ax2.scatter(X[idx].iloc[:, 0], X[idx].iloc[:, 1], c=color[id],
+                        cmap=plt.cm.RdYlBu, edgecolor='black', s=30,
+                        label="Class: "+str(y_label))
+        ax2.set_ylabel("X2")
+        ax2.set_xlabel("X1")
+        ax2.legend()
+        ax2.set_title("Common Decision Surface")
+
+        # Saving Figures
+        fig1.savefig("Q7_Fig1.png")
+        fig2.savefig("Q7_Fig2.png")
+        return fig1, fig2
 
 
 class RandomForestRegressor():
-    def __init__(self, n_estimators=100, criterion='variance', max_depth=100, max_attr=3):
+    def __init__(self, n_estimators=100, criterion='variance', max_depth=10, max_attr=3):
         '''
         :param n_estimators: The number of trees in the forest.
         :param criterion: The function to measure the quality of a split.
