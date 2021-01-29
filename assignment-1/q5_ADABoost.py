fbf29fd3e999d00d0fb0289a43bb0003227ac2f8
varunjain3 <varun.jain@iitgn.ac.in>
2021-01-30 03:35:31 +0530

    changed to sklearn

--- assignment-1/q5_ADABoost.py
+++ assignment-1/q5_ADABoost.py
@@ -12,8 +12,9 @@ from metrics import *
 
 from ensemble.ADABoost import AdaBoostClassifier
 from tree.base import DecisionTree
+from sklearn.tree import DecisionTreeClassifier
 # Or you could import sklearn DecisionTree
-from linearRegression.linearRegression import LinearRegression
+# from linearRegression.linearRegression import LinearRegression
 
 np.random.seed(42)
 
@@ -23,16 +24,19 @@ np.random.seed(42)
 N = 30
 P = 2
 NUM_OP_CLASSES = 2
-n_estimators = 3
+n_estimators = 5
 X = pd.DataFrame(np.abs(np.random.randn(N, P)))
-y = pd.Series(np.random.randint(NUM_OP_CLASSES, size = N), dtype="category")
+y = pd.Series(np.random.randint(NUM_OP_CLASSES, size=N), dtype="category")
+y = y.cat.rename_categories([-1, 1])  # Changing 0 to -1 for adaboost
 
-criteria = 'information_gain'
-tree = DecisionTree(criterion=criteria)
-Classifier_AB = AdaBoostClassifier(base_estimator=tree, n_estimators=n_estimators )
+
+criteria = 'entropy'
+tree = DecisionTreeClassifier
+Classifier_AB = AdaBoostClassifier(
+    base_estimator=tree, n_estimators=n_estimators)
 Classifier_AB.fit(X, y)
 y_hat = Classifier_AB.predict(X)
-[fig1, fig2] = Classifier_AB.plot()
+[fig1, fig2] = Classifier_AB.plot(X, y)
 print('Criteria :', criteria)
 print('Accuracy: ', accuracy(y_hat, y))
 for cls in y.unique():
@@ -40,5 +44,4 @@ for cls in y.unique():
     print('Recall: ', recall(y_hat, y, cls))
 
 
-
-##### AdaBoostClassifier on Iris data set using the entire data set with sepal width and petal width as the two features
+# AdaBoostClassifier on Iris data set using the entire data set with sepal width and petal width as the two features
